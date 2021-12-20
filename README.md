d6cce10193cf859430665dcfa2fdfafd2e68c5a1
Richard Scheglmann <r.scheglmann@gmail.com>
2021-12-20 13:33:30 +0100

    PHP Entwicklungsumgeben mit Docker eingerichtet

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # SQLverine-website
+
+## Docker setup
+- download Docker
+- run `docker-compose up` to start the nginx server
+- you can connect to the maria db database with dbeaver Server-Host: `localhost` Port: `3306`
---
+++ app/public/index.php
@@ -0,0 +1,15 @@
+<?php
+phpinfo();
+
+
+$pdo = new PDO('mysql:dbname=tutorial;host=mysql', 'tutorial', 'secret', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+
+$query = $pdo->query('SHOW VARIABLES like "version"');
+
+$row = $query->fetch();
+
+echo 'MySQL version:' . $row['Value'];
+
+?>
+
+<h1>Hello, World!</h1>
